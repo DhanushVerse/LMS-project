6bb67b98e252e29a7b7433e024b3304508ebcb8c
DhanushSpm <dhanusha272004@gmail.com>
2025-11-06 18:46:19 +0530

    fix: fix for connectDB export to resolve import error

--- app/api/assessments/[Course Id]/route.js
+++ app/api/assessments/[Course Id]/route.js
@@ -1,6 +1,7 @@
 // api/assessment/[courseId]/route.js
 
-import { connectDB } from "../../../utils/db";
+// import { connectDB } from "utils/db"
+import connectDB from "../../../utils/db";
 import Assessment from "../../../models/Assessment";
 
 export async function GET(req, { params }) {
--- app/utils/db.js
+++ app/utils/db.js
@@ -39,4 +39,4 @@ async function connectDB() {
   return cached.conn;
 }
 
-export default connectDB;
+export function connectDB;
