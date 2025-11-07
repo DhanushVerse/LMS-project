d86655dc54b7d1f49212ee3871733682c8bfd8fa
DhanushSpm <dhanusha272004@gmail.com>
2025-11-07 14:13:58 +0530

    fix: Added dynamic='force-dynamic' to assessment API route

--- app/api/assessments/[Course Id]/route.js
+++ app/api/assessments/[Course Id]/route.js
@@ -4,6 +4,9 @@
 import connectDB from "../../../utils/db";
 import Assessment from "../../../models/Assessment";
 
+export const dynamic = 'force-dynamic';
+export const fetchCache = 'default-no-store';
+
 export async function GET(req, { params }) {
   await connectDB();
   const { courseId } = params;
